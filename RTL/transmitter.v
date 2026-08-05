5bda594705e9b15411fa4b6590247b0e8f14b269
Sashidhar077 <sahasra972@gmail.com>
2026-08-05 09:04:58 +0000

    Transmitter

--- RTL/transmitter.v
+++ RTL/transmitter.v
@@ -1,4 +1,145 @@
 
+module transmitter (
+
+    input         i_clk,
+    input         i_rst,
+    input         i_tx_ena_n,
+    input  [9:0] i_data,
+
+    output reg    o_tx_serial,
+    output reg    o_tx_done
+
+);
+
+
+    reg [9:0] shift_reg;
+    reg [3:0] bit_count;
+    reg       tx_valid;
+
+   
+
+    always @(posedge i_clk)
+    begin
+
+        //-------------------------
+        // Reset
+        //-------------------------
+        if(i_rst)
+        begin
+            shift_reg   <= 10'd0;
+            bit_count   <= 4'd0;
+            tx_valid    <= 1'b0;
+
+            o_tx_serial <= 1'b0;
+            o_tx_done   <= 1'b0;
+        end
+
+        //-------------------------
+        // Normal Operation
+        //-------------------------
+        else
+        begin
+
+            // Default
+            o_tx_done <= 1'b0;
+
+            //-------------------------
+            // IDLE State
+            //-------------------------
+            if(!tx_valid)
+            begin
+
+                // Active LOW enable
+                if(!i_tx_ena_n)
+                begin
+                    shift_reg <= i_data;
+                    bit_count <= 4'd0;
+                    tx_valid  <= 1'b1;
+                end
+
+            end
+
+            //-------------------------
+            // TRANSMIT State
+            //-------------------------
+            else
+            begin
+
+                // Output LSB
+                o_tx_serial <= shift_reg[0];
+
+                // Shift right
+                shift_reg <= shift_reg >> 1;
+
+                // Increment bit counter
+                bit_count <= bit_count + 1'b1;
+
+                // Transmission complete?
+                if (bit_count == 4'd9) begin
+                    tx_valid  <= 1'b0;
+                    o_tx_done <= 1'b1;
+                end
+                else begin
+                    bit_count <= bit_count + 1'b1;
+                    shift_reg <= shift_reg >> 1;
+                    o_tx_serial <= shift_reg[0];
+                end
+
+            end
+
+        end
+
+    end
+
+endmodule
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -53,7 +194,9 @@ o_tx_done
 
 
 
-
+Output :
+o_tx_serial
+o_tx_done
 
 
 
---
+++ TESTBENCH/tb_transmitter.v
@@ -0,0 +1,70 @@
+`timescale 1ns/1ps
+
+module tb_transmitter;
+
+    reg         i_clk;
+    reg         i_rst;
+    reg         i_tx_ena_n;
+    reg  [9:0]  i_data;
+
+    wire        o_tx_serial;
+    wire        o_tx_done;
+
+    transmitter DUT
+    (
+        .i_clk(i_clk),
+        .i_rst(i_rst),
+        .i_tx_ena_n(i_tx_ena_n),
+        .i_data(i_data),
+        .o_tx_serial(o_tx_serial),
+        .o_tx_done(o_tx_done)
+    );
+
+    initial
+    begin
+        i_clk = 0;
+        forever #5 i_clk = ~i_clk;
+    end
+
+    initial
+    begin
+        $dumpfile("transmitter.vcd");
+        $dumpvars(0, tb_transmitter);
+    end
+
+    initial
+    begin
+
+        i_rst      = 1'b1;
+        i_tx_ena_n = 1'b1;
+        i_data     = 10'b1010110010;
+
+        #20;
+        i_rst = 1'b0;
+
+        #10;
+        i_tx_ena_n = 1'b0;
+
+        #10;
+        i_tx_ena_n = 1'b1;
+
+        #120;
+
+        $finish;
+
+    end
+
+    initial
+    begin
+        $display("-----------------------------------------------");
+        $display(" Time   TX_EN   Serial   Done");
+        $display("-----------------------------------------------");
+
+        $monitor("%4t      %b        %b       %b",
+                  $time,
+                  i_tx_ena_n,
+                  o_tx_serial,
+                  o_tx_done);
+    end
+
+endmodule
